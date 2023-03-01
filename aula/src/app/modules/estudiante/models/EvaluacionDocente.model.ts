fec230dab77ad516a1474d5611fc4e298712efa7
Pau <paulitayg@hotmail.com>
2023-03-01 16:26:59 -0500

    correcciones

---
+++ aula/src/app/modules/estudiante/models/EvaluacionDocente.model.ts
@@ -0,0 +1,7 @@
+export interface EvaluacionDocente{
+    id: number;
+    curso:number;
+    id_Curso:number;
+    cantidad_evaluacion:number;
+    promedio_evaluacion:number;
+}
---
+++ aula/src/app/modules/estudiante/models/Horario.model.ts
@@ -0,0 +1,7 @@
+export interface Horario{
+    id: number;
+    id_Curso:number;
+    dia:string;
+    hora_inicio:string 
+    hora_fin:string
+}

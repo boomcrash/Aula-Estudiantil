c9be5d6c8c525128a4c84d1b64346d85b79a95e0
Ruben Vera <rubenvera_77@hotmail.com>
2023-03-05 01:02:34 -0500

    Merge con Janco

--- aula/src/environments/environment.ts
+++ aula/src/environments/environment.ts
@@ -9,7 +9,16 @@ urlBAse: "http://167.71.26.121:3000",
         UrlObtenerParalelos:"/api/v1/paralelos/getParalelos",
         UrlObtenerMaterias:"/api/v1/materias/getItemMaterias",
         UrlObtenerDatosCompletos:"/api/v1/usuarios/getUsersCompleteData",
-        UrlObtenerActaByEstudiante:"/api/v1/itemActas/getItemActasByIdEstudiante",
+        UrlObtenerActaByEstudiante:"/api/v1/itemActas/getItemActasByIdEstudiante",                
+        urlObtenerActaEstudiante: "/api/v1/actas/getActaEstudiante",
+        urlObtenerItemMatricula: "/api/v1/matriculas/getItemMatriculasByEstudianteId",
+        urlObtenerOrdenPago: "/api/v1/ordenPagoMatriculas/getItemOrdenPagoMatriculasByMatriculaId",
+        urlObterCursoEstudiante: "/api/v1/cursos/getCursosEstudiante",
+        urlObtenerMaterias: "/api/v1/materias/getItemMateriasByName",
+        urlObtenerActividades: "/api/v1/actividades/getActividadByCurso",
+        urlUpdateEntregas: "/api/v1/entregas/updateEntrega",
+        urlObtenerEntregaActividad: "/api/v1/actividades/entregaActividad",        
+        urlObtenerAsistenciasByIdEstudiante: "/api/v1/asistencias/getAsistenciasByIdEstudiante",
 
         
     }

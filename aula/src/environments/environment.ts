7ea8b0e23d199f3666878c52dc9af0c0769c4b61
emag25 <emely.apraezg@ug.edu.ec>
2023-03-10 17:13:40 -0500

    cambios

--- aula/src/environments/environment.ts
+++ aula/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
-    urlBAse: "http://167.71.26.121:3000",
+    urlBAse: "https://backend.thedeveloper.software",
     pathUrl: {
         urlVerificarUsuarioPassword: "/api/v1/usuarios/verifyUserByUserAndPassword",
         urlObtenerUsuariosPorUser: "/api/v1/usuarios/getUsersByUserName",
